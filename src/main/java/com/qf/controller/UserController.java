328c6608d0e446a54521de92ee22206885c9b4e4
yun <yun@qq.com>
2019-05-17 01:33:50 +0800

    为了老师看,特地拉的分支,老师请过目

--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * @version 1.0
+ * @version 2.0
  * @data 5/16/2019 21:53
  * @user yingyunzhizi
  */
